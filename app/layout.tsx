5999ae5c0813541ef0c607ddb6a512fe6d12515b
AmarjithKarippath <amarjithkarippath@gmail.com>
2025-09-27 20:23:07 +0100

    Updated the fav title

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,8 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Waveify - AI Voice Agent",
+  description: "Created by Amarjith Sudhakar K",
 }
 
 export default function RootLayout({
